a7da2be47e83fae09e74dbe8b3005696c467bafa
Ryan Shiu <ryan.shiu23@ycdsbk12.ca>
2020-12-07 18:38:45 +0000

    live hack

--- problem1.py
+++ problem1.py
@@ -1,1 +1,18 @@
+'''
+-------------------------------------------------------------------------------
+Name:	 problem1.py
+Purpose:	Converting Celsius into fahrenheit. 
 
+Author:	Shiu.R
+
+Created:	07/12/2020
+------------------------------------------------------------------------------
+'''
+# welcome the user to the software
+print("******* Welcome to the Temperature converter (from celsius to fahrenheit) ********")
+# get the celsius
+celsius = float(input("Enter the degrees celsius: "))
+# calculate the fahrenheit
+fahrenheit = 9/5*celsius + 32
+# tell the user the fahrenheit of celcius inputted
+print("The fahrenheit of " , str(celsius) , "degrees celsius is" , str(fahrenheit) , "fahrenheit" )
--- problem2.py
+++ problem2.py
@@ -1,1 +1,19 @@
+'''
+-------------------------------------------------------------------------------
+Name:	 problem2.py
+Purpose:	To evenly distribute the chicken wings with the students and find out how many chicken wings are leftover or Mr.Fabroa
 
+Author:	Shiu.R
+
+Created:	07/12/2020
+------------------------------------------------------------------------------
+'''
+# Ask user to input the amount of chicken wings and the anount of students 
+chicken_wings = int(input("How many chicken wings are there?: "))
+students = int(input("How many students are in your class?: "))
+# Calculate how many pieces of chicken wings each studen will get
+student_chicken = chicken_wings // students
+# Calculate how much leftover chicken wings are left for Mr.Fabroa
+fabroa_chicken = chicken_wings % students
+# Output the results
+print("Each student will receive:" , str(student_chicken) , "chicken wing(s)" , "and Mr.Fabroa will receive:", str(fabroa_chicken) , ("chicken wing(s)"))
